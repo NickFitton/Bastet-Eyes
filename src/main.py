5be4785ba5ac81508f7db8e9c3ce2d0582c53a13
NickFitton <github@nfitton.com>
2018-11-13 08:37:10 +0000

    Revert to using picamera

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from os import getcwd, remove
 
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 from math import floor
-from time import time, sleep
+from time import time
 import cv2
 import requests
 
@@ -17,8 +19,10 @@ current_path = getcwd()
 initialization_time = floor(time())
 
 fgbg = cv2.createBackgroundSubtractorMOG2()
-
-cameraFeed = cv2.VideoCapture('rtsp://192.168.0.15:554/user=user2_password=Lostinblue2_channel=2_stream=0.sdp')
+camera = PiCamera()
+camera.resolution = (1640, 1232)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera)
 
 minContourArea = 4000
 captured_entities = []
@@ -55,53 +59,45 @@ def save_to_local(image, entry_time, exit_time):
 
 
 start_time = time()
-while True:
-    frame = cameraFeed.read()[1]
-    if frame is None:
-        cameraFeed = cv2.VideoCapture('rtsp://192.168.0.15:554/user=user2_password=Lostinblue2_channel=2_stream=0.sdp')
-        log("Stream restarted")
-    elif time() - start_time < 5:
-        log("Waiting for camera exposure")
-        sleep(5)
-    else:
-        original_frame = frame.copy()
+for rawCapture in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    frame = rawCapture.array
+    original_frame = frame.copy()
 
-        preexisting_entities = captured_entities.copy()
-        mog_contours = background_diff_mog_2(frame)
+    preexisting_entities = captured_entities.copy()
+    mog_contours = background_diff_mog_2(frame)
 
-        for found_contour in mog_contours:
-            if cv2.contourArea(found_contour) >= minContourArea:
-                x, y, w, h = cv2.boundingRect(found_contour)
-                newEntity = Entity(x, y, original_frame[y:y + h, x:x + w])
-                entity_exists = False
-                for preexisting_entity in preexisting_entities:
-                    if (not entity_exists) and preexisting_entity.is_entity(newEntity):
-                        preexisting_entity.update(newEntity)
-                        entity_exists = True
-                        break
+    for found_contour in mog_contours:
+        if cv2.contourArea(found_contour) >= minContourArea:
+            x, y, w, h = cv2.boundingRect(found_contour)
+            newEntity = Entity(x, y, original_frame[y:y + h, x:x + w])
+            entity_exists = False
+            for preexisting_entity in preexisting_entities:
+                if (not entity_exists) and preexisting_entity.is_entity(newEntity):
+                    preexisting_entity.update(newEntity)
+                    entity_exists = True
+                    break
 
-                if not entity_exists:
-                    log("Adding new entity")
-                    captured_entities.append(newEntity)
+            if not entity_exists:
+                log("Adding new entity")
+                captured_entities.append(newEntity)
 
-        for existingEntity in captured_entities:
-            if time() - existingEntity.last_active > 5:
-                log("Entity '{}' became inactive, reporting".format(existingEntity.id))
-                save_to_server(existingEntity.best_image, existingEntity.first_active, existingEntity.last_active,
-                               existingEntity.image_time)
-                captured_entities.remove(existingEntity)
-            else:
-                cv2.rectangle(frame,
-                              (existingEntity.x, existingEntity.y),
-                              (existingEntity.x + existingEntity.image.shape[1],
-                               existingEntity.y + existingEntity.image.shape[0]),
-                              (existingEntity.b, existingEntity.g, existingEntity.r), 2)
-    #    cv2.imshow("Image", frame)
+    for existingEntity in captured_entities:
+        if time() - existingEntity.last_active > 5:
+            log("Entity '{}' became inactive, reporting".format(existingEntity.id))
+            save_to_server(existingEntity.best_image, existingEntity.first_active, existingEntity.last_active,
+                           existingEntity.image_time)
+            captured_entities.remove(existingEntity)
+        else:
+            cv2.rectangle(frame,
+                          (existingEntity.x, existingEntity.y),
+                          (existingEntity.x + existingEntity.image.shape[1],
+                           existingEntity.y + existingEntity.image.shape[0]),
+                          (existingEntity.b, existingEntity.g, existingEntity.r), 2)
 
-        # rawCapture.truncate(0)
-        key = cv2.waitKey(1) & 0xFF
+    rawCapture.truncate(0)
+    key = cv2.waitKey(1) & 0xFF
 
-        if key == ord("q"):
-            break
-        elif key == ord("r"):
-            captured_entities = []
+    if key == ord("q"):
+        break
+    elif key == ord("r"):
+        captured_entities = []
